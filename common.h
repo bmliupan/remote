e7111e6c680d5822681264c196884581da2797b6
liupan <bmliupan@qq.com>
2020-09-12 21:48:56 +0800

    v1.0 release

---
+++ common.h
@@ -0,0 +1,76 @@
+#ifndef COMMON_H
+#define COMMON_H
+#include <QApplication>
+
+enum FlagType {
+    normalFlag,     //机顶盒按键输入标记
+    learnFlag,      //学习按键输入标记
+    setFlag,        //设置键输入标记
+    invalidFlag,    //无效输入标记
+    nonInputFlag    //没有输入
+};
+
+enum ErrorType {
+    E_OVER_LENGTH,      //输入字符超长
+    E_OVER_LEARNKEY    //超出最大可支持学习按键数
+};
+
+bool toggleState = true;
+bool display_flag = true; //序号显示与否标记位
+uint8_t edit_flag = normalFlag; //编辑模式标记
+uint8_t keyValue[66] = {0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,\
+                       0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,\
+                       0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,\
+                       0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,\
+                       0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,\
+                       0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,\
+                       0xff,0xff,0xff,0xff,0xff,0xff}; //保存键值
+
+uint8_t keyFlag[66] = {nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,\
+                       nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,\
+                       nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,\
+                       nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,\
+                       nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,\
+                       nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,\
+                       nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,\
+                       nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,\
+                       nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,\
+                       nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,\
+                       nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,\
+                       nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,\
+                       nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,nonInputFlag,\
+                       nonInputFlag}; //保存按键模式标记
+
+uint8_t userCode[4] = {0};
+
+uint8_t translate[66] = {
+    55,56,57,58,59,60,61,62,63,64,65,\
+    45,46,47,48,49,50,51,52,53,54,\
+    36,37,38,39,40,41,42,43,44,\
+    28,29,30,31,32,33,34,35,\
+    21,22,23,24,25,26,27,\
+    15,16,17,18,19,20,\
+    10,11,12,13,14,\
+     6, 7, 8, 9,\
+     3, 4, 5,\
+     1, 2,\
+     0
+};
+
+uint8_t translist[66] = {
+    65,\
+    63,64,\
+    60,61,62,\
+    56,57,58,59,\
+    51,52,53,54,55,\
+    45,46,47,48,49,50,\
+    38,39,40,41,42,43,44,\
+    30,31,32,33,34,35,36,37,\
+    21,22,23,24,25,26,27,28,29,\
+    11,12,13,14,15,16,17,18,19,20,\
+     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10
+};
+
+const int MaxLearnKeyNum = 7;
+
+#endif // COMMON_H
---
+++ mainwindow.h
@@ -0,0 +1,95 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+public slots:
+    void on_pushButton_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+
+    //行编辑输入
+    void on_lineEdit_001_editingFinished();
+    void on_lineEdit_002_editingFinished();
+    void on_lineEdit_003_editingFinished();
+    void on_lineEdit_004_editingFinished();
+    void on_lineEdit_005_editingFinished();
+    void on_lineEdit_006_editingFinished();
+    void on_lineEdit_007_editingFinished();
+    void on_lineEdit_008_editingFinished();
+    void on_lineEdit_009_editingFinished();
+    void on_lineEdit_010_editingFinished();
+    void on_lineEdit_011_editingFinished();
+    void on_lineEdit_012_editingFinished();
+    void on_lineEdit_013_editingFinished();
+    void on_lineEdit_014_editingFinished();
+    void on_lineEdit_015_editingFinished();
+    void on_lineEdit_016_editingFinished();
+    void on_lineEdit_017_editingFinished();
+    void on_lineEdit_018_editingFinished();
+    void on_lineEdit_019_editingFinished();
+    void on_lineEdit_020_editingFinished();
+    void on_lineEdit_021_editingFinished();
+    void on_lineEdit_022_editingFinished();
+    void on_lineEdit_023_editingFinished();
+    void on_lineEdit_024_editingFinished();
+    void on_lineEdit_025_editingFinished();
+    void on_lineEdit_026_editingFinished();
+    void on_lineEdit_027_editingFinished();
+    void on_lineEdit_028_editingFinished();
+    void on_lineEdit_029_editingFinished();
+    void on_lineEdit_030_editingFinished();
+    void on_lineEdit_031_editingFinished();
+    void on_lineEdit_032_editingFinished();
+    void on_lineEdit_033_editingFinished();
+    void on_lineEdit_034_editingFinished();
+    void on_lineEdit_035_editingFinished();
+    void on_lineEdit_036_editingFinished();
+    void on_lineEdit_037_editingFinished();
+    void on_lineEdit_038_editingFinished();
+    void on_lineEdit_039_editingFinished();
+    void on_lineEdit_040_editingFinished();
+    void on_lineEdit_041_editingFinished();
+    void on_lineEdit_042_editingFinished();
+    void on_lineEdit_043_editingFinished();
+    void on_lineEdit_044_editingFinished();
+    void on_lineEdit_045_editingFinished();
+    void on_lineEdit_046_editingFinished();
+    void on_lineEdit_047_editingFinished();
+    void on_lineEdit_048_editingFinished();
+    void on_lineEdit_049_editingFinished();
+    void on_lineEdit_050_editingFinished();
+    void on_lineEdit_051_editingFinished();
+    void on_lineEdit_052_editingFinished();
+    void on_lineEdit_053_editingFinished();
+    void on_lineEdit_054_editingFinished();
+    void on_lineEdit_055_editingFinished();
+    void on_lineEdit_056_editingFinished();
+    void on_lineEdit_057_editingFinished();
+    void on_lineEdit_058_editingFinished();
+    void on_lineEdit_059_editingFinished();
+    void on_lineEdit_060_editingFinished();
+    void on_lineEdit_061_editingFinished();
+    void on_lineEdit_062_editingFinished();
+    void on_lineEdit_063_editingFinished();
+    void on_lineEdit_064_editingFinished();
+    void on_lineEdit_065_editingFinished();
+    void on_lineEdit_066_editingFinished();
+    void on_lineEdit_070_editingFinished();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
